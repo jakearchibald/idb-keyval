c5231e0a059ce720a6772fc329d98f7fa312e12d
Benny Powers <web@bennypowers.com>
2018-03-08 22:35:49 +0200

    Adds iife-fied browser output in dist

---
+++ dist/idb-keyval-browser.js
@@ -0,0 +1,85 @@
+var idbKeyval = (function () {
+'use strict';
+
+var db;
+
+function getDB() {
+  if (!db) {
+    db = new Promise(function(resolve, reject) {
+      var openreq = indexedDB.open('keyval-store', 1);
+
+      openreq.onerror = function() {
+        reject(openreq.error);
+      };
+
+      openreq.onupgradeneeded = function() {
+        // First time setup: create an empty object store
+        openreq.result.createObjectStore('keyval');
+      };
+
+      openreq.onsuccess = function() {
+        resolve(openreq.result);
+      };
+    });
+  }
+  return db;
+}
+
+function withStore(type, callback) {
+  return getDB().then(function(db) {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction('keyval', type);
+      transaction.oncomplete = function() {
+        resolve();
+      };
+      transaction.onerror = function() {
+        reject(transaction.error);
+      };
+      callback(transaction.objectStore('keyval'));
+    });
+  });
+}
+
+var idbKeyval = {
+  get: function(key) {
+    var req;
+    return withStore('readonly', function(store) {
+      req = store.get(key);
+    }).then(function() {
+      return req.result;
+    });
+  },
+  set: function(key, value) {
+    return withStore('readwrite', function(store) {
+      store.put(value, key);
+    });
+  },
+  delete: function(key) {
+    return withStore('readwrite', function(store) {
+      store.delete(key);
+    });
+  },
+  clear: function() {
+    return withStore('readwrite', function(store) {
+      store.clear();
+    });
+  },
+  keys: function() {
+    var keys = [];
+    return withStore('readonly', function(store) {
+      // This would be store.getAllKeys(), but it isn't supported by Edge or Safari.
+      // And openKeyCursor isn't supported by Safari.
+      (store.openKeyCursor || store.openCursor).call(store).onsuccess = function() {
+        if (!this.result) return;
+        keys.push(this.result.key);
+        this.result.continue();
+      };
+    }).then(function() {
+      return keys;
+    });
+  }
+};
+
+return idbKeyval;
+
+}());
--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,15 @@
 export default {
   input: 'idb-keyval.js',
   output: [{
-    file: 'dist/idb-keyval-cjs.js',
-    format: 'cjs',
+    file: 'dist/idb-keyval-browser.js',
+    format: 'iife',
     name: 'idbKeyval',
   }, {
     file: 'dist/idb-keyval.mjs',
     format: 'es',
+  }, {
+    file: 'dist/idb-keyval-cjs.js',
+    format: 'cjs',
+    name: 'idbKeyval',
   }]
 }
