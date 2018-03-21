e243d1357aef7e116e2596b824a9ae8ed8fcb1c6
Jake Archibald <jaffathecake@gmail.com>
2018-03-21 08:31:36 +0000

    Converting to typescript

---
+++ idb-keyval.ts
@@ -0,0 +1,77 @@
+let db: Promise<IDBDatabase>;
+
+function getDB() {
+  if (!db) {
+    db = new Promise((resolve, reject) => {
+      const openreq = indexedDB.open('keyval-store', 1);
+
+      openreq.onerror = () => {
+        reject(openreq.error);
+      };
+
+      openreq.onupgradeneeded = () => {
+        // First time setup: create an empty object store
+        openreq.result.createObjectStore('keyval');
+      };
+
+      openreq.onsuccess = () => {
+        resolve(openreq.result);
+      };
+    });
+  }
+  return db;
+}
+
+function withStore(type: IDBTransactionMode, callback: ((store: IDBObjectStore) => void)): Promise<void> {
+  return getDB().then(db => {
+    return new Promise<void>((resolve, reject) => {
+      const transaction = db.transaction('keyval', type);
+      transaction.oncomplete = () => {
+        resolve();
+      };
+      transaction.onerror = () => {
+        reject(transaction.error);
+      };
+      callback(transaction.objectStore('keyval'));
+    });
+  });
+}
+
+export function get<Type>(key: IDBValidKey): Promise<Type> {
+  let req: IDBRequest;
+  return withStore('readonly', store => {
+    req = store.get(key);
+  }).then(() => req.result);
+}
+
+export function set<Type>(key: IDBValidKey, value: any): Promise<void> {
+  return withStore('readwrite', store => {
+    store.put(value, key);
+  });
+}
+
+export function del(key: IDBValidKey): Promise<void> {
+  return withStore('readwrite', store => {
+    store.delete(key);
+  });
+}
+
+export function clear(): Promise<void> {
+  return withStore('readwrite', function (store) {
+    store.clear();
+  });
+}
+
+export function keys(): Promise<IDBValidKey[]> {
+  const keys: IDBValidKey[] = [];
+
+  return withStore('readonly', function (store) {
+    // This would be store.getAllKeys(), but it isn't supported by Edge or Safari.
+    // And openKeyCursor isn't supported by Safari.
+    (store.openKeyCursor || store.openCursor).call(store).onsuccess = function() {
+      if (!this.result) return;
+      keys.push(this.result.key);
+      this.result.continue()
+    };
+  }).then(() => keys);
+}
--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,15 @@
+import typescript from 'rollup-plugin-typescript2';
+
 export default {
-  input: 'idb-keyval.js',
+  input: 'idb-keyval.ts',
+  plugins: [typescript()],
   output: [{
     file: 'dist/idb-keyval-iife.js',
     format: 'iife',
-    name: 'idbKeyval',
+    name: 'idbKeyval'
   }, {
     file: 'dist/idb-keyval-cjs.js',
     format: 'cjs',
-    name: 'idbKeyval',
+    name: 'idbKeyval'
   }]
-}
+};
