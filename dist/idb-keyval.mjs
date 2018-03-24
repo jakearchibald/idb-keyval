a5c35f75175e0ee33ba4b11636b9639d04ba11b5
Dan Fabulich <dan.fabulich@redfin.com>
2018-03-23 21:12:14 -0700

    Export to ES5 for IE11 support.
    This adds ~300 bytes to the ungzipped idb-keyval-iife.min.js, adding
    ~30 bytes to the gzipped version.
    
    With this change, idb-keyval-iife.js parses without syntax error on
    IE11, but fails during execution for lack of a global Promise.
    
    Users desiring IE11 support would need to provide their own Promise
    polyfill.

--- dist/idb-keyval.mjs
+++ dist/idb-keyval.mjs
@@ -1,55 +1,64 @@
-class Store {
-    constructor(dbName = 'keyval-store', storeName = 'keyval') {
+var Store = /** @class */ (function () {
+    function Store(dbName, storeName) {
+        if (dbName === void 0) { dbName = 'keyval-store'; }
+        if (storeName === void 0) { storeName = 'keyval'; }
         this.storeName = storeName;
-        this._dbp = new Promise((resolve, reject) => {
-            const openreq = indexedDB.open(dbName, 1);
-            openreq.onerror = () => reject(openreq.error);
-            openreq.onsuccess = () => resolve(openreq.result);
+        this._dbp = new Promise(function (resolve, reject) {
+            var openreq = indexedDB.open(dbName, 1);
+            openreq.onerror = function () { return reject(openreq.error); };
+            openreq.onsuccess = function () { return resolve(openreq.result); };
             // First time setup: create an empty object store
-            openreq.onupgradeneeded = () => {
+            openreq.onupgradeneeded = function () {
                 openreq.result.createObjectStore(storeName);
             };
         });
     }
-    _withIDBStore(type, callback) {
-        return this._dbp.then(db => new Promise((resolve, reject) => {
-            const transaction = db.transaction(this.storeName, type);
-            transaction.oncomplete = () => resolve();
-            transaction.onabort = transaction.onerror = () => reject(transaction.error);
-            callback(transaction.objectStore(this.storeName));
-        }));
-    }
-}
-let store;
+    Store.prototype._withIDBStore = function (type, callback) {
+        var _this = this;
+        return this._dbp.then(function (db) { return new Promise(function (resolve, reject) {
+            var transaction = db.transaction(_this.storeName, type);
+            transaction.oncomplete = function () { return resolve(); };
+            transaction.onabort = transaction.onerror = function () { return reject(transaction.error); };
+            callback(transaction.objectStore(_this.storeName));
+        }); });
+    };
+    return Store;
+}());
+var store;
 function getDefaultStore() {
     if (!store)
         store = new Store();
     return store;
 }
-function get(key, store = getDefaultStore()) {
-    let req;
-    return store._withIDBStore('readonly', store => {
+function get(key, store) {
+    if (store === void 0) { store = getDefaultStore(); }
+    var req;
+    return store._withIDBStore('readonly', function (store) {
         req = store.get(key);
-    }).then(() => req.result);
+    }).then(function () { return req.result; });
 }
-function set(key, value, store = getDefaultStore()) {
-    return store._withIDBStore('readwrite', store => {
+function set(key, value, store) {
+    if (store === void 0) { store = getDefaultStore(); }
+    return store._withIDBStore('readwrite', function (store) {
         store.put(value, key);
     });
 }
-function del(key, store = getDefaultStore()) {
-    return store._withIDBStore('readwrite', store => {
+function del(key, store) {
+    if (store === void 0) { store = getDefaultStore(); }
+    return store._withIDBStore('readwrite', function (store) {
         store.delete(key);
     });
 }
-function clear(store = getDefaultStore()) {
-    return store._withIDBStore('readwrite', store => {
+function clear(store) {
+    if (store === void 0) { store = getDefaultStore(); }
+    return store._withIDBStore('readwrite', function (store) {
         store.clear();
     });
 }
-function keys(store = getDefaultStore()) {
-    const keys = [];
-    return store._withIDBStore('readonly', store => {
+function keys(store) {
+    if (store === void 0) { store = getDefaultStore(); }
+    var keys = [];
+    return store._withIDBStore('readonly', function (store) {
         // This would be store.getAllKeys(), but it isn't supported by Edge or Safari.
         // And openKeyCursor isn't supported by Safari.
         (store.openKeyCursor || store.openCursor).call(store).onsuccess = function () {
@@ -58,7 +67,7 @@ function keys(store = getDefaultStore()) {
             keys.push(this.result.key);
             this.result.continue();
         };
-    }).then(() => keys);
+    }).then(function () { return keys; });
 }
 
 export { Store, get, set, del, clear, keys };
