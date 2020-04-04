6f5e0b2d5c883561d57d9a151a339523ce00722b
liudaoyu <liudaoyu@gmail.com>
2020-04-05 00:56:15 +0800

    put store ops in a command class

--- idb-keyval.ts
+++ idb-keyval.ts
@@ -1,3 +1,4 @@
+
 export class Store {
   readonly _dbp: Promise<IDBDatabase>;
 
@@ -31,41 +32,48 @@ function getDefaultStore() {
   return store;
 }
 
-export function get<Type>(key: IDBValidKey, store = getDefaultStore()): Promise<Type> {
-  let req: IDBRequest;
-  return store._withIDBStore('readonly', store => {
-    req = store.get(key);
-  }).then(() => req.result);
+export class StoreCommand {
+  constructor(public store: Store = getDefaultStore()) {}
+
+ get<Type>(key: IDBValidKey): Promise<Type> {
+    let req: IDBRequest;
+    return this.store._withIDBStore('readonly', store => {
+      req = store.get(key);
+    }).then(() => req.result);
+  }
+  
+  set(key: IDBValidKey, value: any): Promise<void> {
+    return this.store._withIDBStore('readwrite', store => {
+      store.put(value, key);
+    });
+  }
+  
+  del(key: IDBValidKey): Promise<void> {
+    return this.store._withIDBStore('readwrite', store => {
+      store.delete(key);
+    });
+  }
+  
+  clear(): Promise<void> {
+    return this.store._withIDBStore('readwrite', store => {
+      store.clear();
+    });
+  }
+  
+  keys(): Promise<IDBValidKey[]> {
+    const keys: IDBValidKey[] = [];
+  
+    return this.store._withIDBStore('readonly', store => {
+      // This would be store.getAllKeys(), but it isn't supported by Edge or Safari.
+      // And openKeyCursor isn't supported by Safari.
+      (store.openKeyCursor || store.openCursor).call(store).onsuccess = function() {
+        if (!this.result) return;
+        keys.push(this.result.key);
+        this.result.continue()
+      };
+    }).then(() => keys);
+  }
+
 }
 
-export function set(key: IDBValidKey, value: any, store = getDefaultStore()): Promise<void> {
-  return store._withIDBStore('readwrite', store => {
-    store.put(value, key);
-  });
-}
 
-export function del(key: IDBValidKey, store = getDefaultStore()): Promise<void> {
-  return store._withIDBStore('readwrite', store => {
-    store.delete(key);
-  });
-}
-
-export function clear(store = getDefaultStore()): Promise<void> {
-  return store._withIDBStore('readwrite', store => {
-    store.clear();
-  });
-}
-
-export function keys(store = getDefaultStore()): Promise<IDBValidKey[]> {
-  const keys: IDBValidKey[] = [];
-
-  return store._withIDBStore('readonly', store => {
-    // This would be store.getAllKeys(), but it isn't supported by Edge or Safari.
-    // And openKeyCursor isn't supported by Safari.
-    (store.openKeyCursor || store.openCursor).call(store).onsuccess = function() {
-      if (!this.result) return;
-      keys.push(this.result.key);
-      this.result.continue()
-    };
-  }).then(() => keys);
-}
