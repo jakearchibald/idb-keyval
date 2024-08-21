af391aaa2cde4410008615d6b4f8b00124528a4c
Zarko Hristovski <zarko@timegrip.no>
2024-08-21 09:54:32 +0200

    Revert "dist"
    This reverts commit beade592fdc9276ec52c918e66d98b167f6334f2.

--- dist/util.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function promisifyRequest<T = undefined>(request: IDBRequest<T> | IDBTransaction): Promise<T>;
-export declare function openDatabase(dbName: string, storeName: string, retry?: boolean): Promise<IDBDatabase>;
-export declare function closeDatabase(dbName: string): Promise<void>;
